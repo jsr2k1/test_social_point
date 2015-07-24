265a1ac56e3672b727ff4564741937e778c19c11
aratingastudios <aratingadevelopers@gmail.com>
2015-07-24 13:08:18 +0200

    Escena de menú y cambio de plataforma

---
+++ Assets/Scripts/ButtonsController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonsController : MonoBehaviour
+{
+	public void OnPlayButtonPressed()
+	{
+		Application.LoadLevel("01 MainScene");
+	}
+}
